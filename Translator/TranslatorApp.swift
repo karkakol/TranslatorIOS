5d00fc1748b721804537e7d1e23e6f3909ff1f7d
Karol Kąkol <kakol.karol00@gmail.com>
2026-07-24 20:37:22 +0200

    Initial Commit

---
+++ Translator/TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatorApp.swift
+//  Translator
+//
+//  Created by Karol Kakol on 24/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
